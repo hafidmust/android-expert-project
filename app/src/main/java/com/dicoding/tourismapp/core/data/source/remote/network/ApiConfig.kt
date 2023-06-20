5c2b9fff51ab64881c57b3ca9dcf46759a85354e
hafidmust <hafidalimustaqim13@gmail.com>
2023-06-20 22:28:43 +0700

    menambahkan chucker (modul clean arch)

---
+++ app/src/main/java/com/dicoding/tourismapp/core/data/source/remote/network/ApiConfig.kt
@@ -0,0 +1,41 @@
+package com.dicoding.tourismapp.core.data.source.remote.network
+
+import android.content.Context
+import com.chuckerteam.chucker.api.ChuckerCollector
+import com.chuckerteam.chucker.api.ChuckerInterceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+class ApiConfig(context: Context) {
+
+    private fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addNetworkInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .connectTimeout(120, TimeUnit.SECONDS)
+            .readTimeout(120, TimeUnit.SECONDS)
+            .build()
+    }
+
+    val okHttpClientChucker = OkHttpClient.Builder()
+        .addInterceptor(
+            ChuckerInterceptor.Builder(context)
+                .collector(ChuckerCollector(context))
+                .maxContentLength(250000L)
+                .redactHeaders(emptySet())
+                .alwaysReadResponseBody(false)
+                .build()
+        )
+        .build()
+
+    fun provideApiService(): ApiService {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://tourism-api.dicoding.dev/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(provideOkHttpClient())
+            .build()
+        return retrofit.create(ApiService::class.java)
+    }
+}
--- app/src/main/java/com/dicoding/tourismapp/core/di/Injection.kt
+++ app/src/main/java/com/dicoding/tourismapp/core/di/Injection.kt
@@ -7,6 +7,7 @@ import com.dicoding.tourismapp.core.data.source.local.room.TourismDatabase
 
 import com.dicoding.tourismapp.core.data.TourismRepository
 import com.dicoding.tourismapp.core.data.source.remote.RemoteDataSource
+import com.dicoding.tourismapp.core.data.source.remote.network.ApiConfig
 import com.dicoding.tourismapp.core.domain.repository.ITourismRepository
 import com.dicoding.tourismapp.core.domain.usecase.TourismInteractor
 import com.dicoding.tourismapp.core.domain.usecase.TourismUseCase
@@ -16,8 +17,8 @@ import com.dicoding.tourismapp.core.utils.JsonHelper
 object Injection {
     fun provideRepository(context: Context): ITourismRepository {
         val database = TourismDatabase.getInstance(context)
-
-        val remoteDataSource = RemoteDataSource.getInstance(JsonHelper(context))
+        val api = ApiConfig(context)
+        val remoteDataSource = RemoteDataSource.getInstance(api.provideApiService())
         val localDataSource = LocalDataSource.getInstance(database.tourismDao())
         val appExecutors = AppExecutors()
 
